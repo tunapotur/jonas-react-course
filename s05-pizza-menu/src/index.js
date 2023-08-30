3fcce99d39ca44d9a28e455555035df3fcc6b4c2
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-08-30 22:19:09 +0300

    s05

--- s05-pizza-menu/src/index.js
+++ s05-pizza-menu/src/index.js
@@ -94,7 +94,8 @@ function Menu() {
         <p>We're still working on our menu. Please come back later :)</p>
       )}
 
-      {/* <Pizza
+      {/* 
+      <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
@@ -105,7 +106,8 @@ function Menu() {
         ingredients="Tomato, mushrooms"
         price={12}
         photoName="pizzas/funghi.jpg"
-      /> */}
+      /> 
+      */}
     </main>
   );
 }
